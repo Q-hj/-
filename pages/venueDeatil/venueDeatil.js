ed0fdfa8b8ed2d46d0ec8cdd5d2c9235cc1a046a
LuckyCat <1485642186@qq.com>
2022-07-03 23:31:23 +0800

    :sparkles:新增详情页面

---
+++ pages/venueDeatil/venueDeatil.js
@@ -0,0 +1,39 @@
+var app = getApp();
+import { formatDate, formatIntDate } from "/utils/common";
+import user from "/utils/User/user";
+Page({
+  data: {
+    baseUrl: app.globalData.fileBaseUrl,
+    testImg:
+      "https://xfcgyy.119.gov.cn/manage/img/obs?token=103945fdadafd4e0b08701f148d1e850&url=fb/2021-08-10/04f9c81a-ddd6-4ac5-991f-25fe37b92ebe.jpg",
+  },
+  onLoad({ fireBrigadeId }) {
+    fireBrigadeId = 4724; //测试
+    app.login().then(() =>
+      app
+        .get("/fireBrigades/getDetails", { fireBrigadeId })
+        .then((venueDetail) =>
+          this.setData({
+            venueDetail,
+            imgs:
+              [
+                this.data.testImg,
+                this.data.testImg,
+                this.data.testImg,
+                this.data.testImg,
+              ] || venueDetail.imgs.split(";"),
+          })
+        )
+    );
+  },
+  handleNav() {
+    const { longitude, latitude, fireBrigadeName, address } =
+      this.data.venueDetail;
+    my.openLocation({
+      longitude,
+      latitude,
+      name: fireBrigadeName,
+      address,
+    });
+  },
+});
